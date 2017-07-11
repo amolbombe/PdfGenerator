42dafad38ca00dd344ba375239925254310abbf3
amol <amolbombe26@gmail.com>
2017-07-11 17:51:48 +0530

    Initial Commit

---
+++ SoESign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoESign
+//
+//  Created by Amol Bombe on 11/07/17.
+//  Copyright © 2017 softcell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
